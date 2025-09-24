665ec4a08985b912ab1477118485f503e541d58e
myadammounika1408-byte <myadammounika1408@gmail.com>
2025-09-24 09:34:58 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,72 @@
+class Lab1P1{
+	public static void main(String...args){
+		byte b=15;
+		Byte bb=b;
+		
+		//int x=200;
+		//byte b=(byte)x;
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(bb.MIN_VALUE);
+		System.out.println(bb.SIZE);
+		System.out.println(bb.TYPE);
+		System.out.println(bb.BYTES);
+		
+		System.out.println("--------------INTEGER-------------");
+		int x=200;
+		Integer it=x;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(it.MIN_VALUE);
+		System.out.println(it.SIZE);
+		System.out.println(it.TYPE);
+		System.out.println(it.INTEGERS);
+		
+		System.out.println("----------SHORT-----------------");
+		short x=200;
+		Short ss=x;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(ss.MIN_VALUE);
+		System.out.println(ss.SIZE);
+		System.out.println(ss.TYPE);
+		System.out.println(ss.SHORTS);
+		
+		System.out.println("---------LONG---------------");
+		long x=200;
+		Long ll=x;
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(ll.MIN_VALUE);
+		System.out.println(ll.SIZE);
+		System.out.println(ll.TYPE);
+		System.out.println(ll.LONGS);
+		
+		System.out.println("--------FLOAT---------------");
+		float x=200;
+		Float ff=x;
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(ff.MIN_VALUE);
+		System.out.println(ff.SIZE);
+		System.out.println(ff.TYPE);
+		System.out.println(ff.FLOATS);
+		
+		System.out.println("--------DOUBLE------------");
+		double x=200;
+		Double dd=x;
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(dd.MIN_VALUE);
+		System.out.println(dd.SIZE);
+		System.out.println(dd.TYPE);
+		System.out.println(dd.DOUBLES);
+		
+		System.out.println("-------BOOLEAN-----------");
+		boolean x=200;
+		Boolean bol=x;
+		System.out.println(Boolean.MAX_VALUE);
+		System.out.println(bol.MIN_VALUE);
+		System.out.println(bol.SIZE);
+		System.out.println(bol.TYPE);
+		System.out.println(bol.BOOLEANS);
+		
+		
+}	
+	
+				
