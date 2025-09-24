930eb58fad3396237dd8ad237886aeac330da941
myadammounika1408-byte <myadammounika1408@gmail.com>
2025-09-24 12:43:26 +0530

    Add files via upload

---
+++ LAB4/Operations.java
@@ -0,0 +1,7 @@
+package Student;
+
+interface Operations{
+	abstract void register(Student std);
+	abstract void logIn(String name,String password);
+	abstract void exit();
+}
---
+++ LAB4/StudentMain.java
@@ -0,0 +1,20 @@
+package Student;
+import java.util.Scanner;
+import java.io.*;
+
+class StudentMain implements Operations{
+		static String name,id,branch,password;
+		Scanner s = new Scanner(System.in);
+		System.out.println("==================\n Welcome to SMS \n==================);
+		public void register(Student std){
+			System.out.println("Menu\n1.Registration\n2.logIn\n3.Exit");
+			System.out.println("Select Menu");
+		}
+		public void logIn(String name,String password){
+		
+		}
+		public void exit(){
+		
+		}
+	}
+	public 
